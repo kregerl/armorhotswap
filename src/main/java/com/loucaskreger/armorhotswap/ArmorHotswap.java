b332374c12829fe7a8c17f8b91615b9066b4029b
kregerl <loucaskreger@gmail.com>
2020-11-27 21:14:25 -0500

    completed 1.16 port

---
+++ src/main/java/com/loucaskreger/armorhotswap/ArmorHotswap.java
@@ -0,0 +1,27 @@
+package com.loucaskreger.armorhotswap;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(ArmorHotswap.MOD_ID)
+public class ArmorHotswap {
+	public static final String MOD_ID = "armorhotswap";
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	public ArmorHotswap() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupCommon);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
+
+	}
+
+	private void setupCommon(final FMLCommonSetupEvent event) {
+	}
+
+	private void setupClient(final FMLClientSetupEvent event) {
+	}
+
+}
